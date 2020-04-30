52ae2b9be5992669b50ea291f5de6dd199ebab0a
Barnabé Geffroy <barnabe.geffroy@psl.eu>
2020-04-30 09:59:44 +0200

    allows just log + comsetics

---
+++ script/cibuild.sh
@@ -0,0 +1,49 @@
+set -e
+REPO="receive_plaquette"
+FILE_LOG=out.log
+FILE_PDF=out.pdf
+DEPLOY_REPO="https://${MY_DOC}@github.com/barnabegeffroy/${REPO}.git"
+function main {
+	clean
+	get_current_doc
+	build_doc
+	if [ -z "$TRAVIS_PULL_REQUEST" ]; then
+	   	echo "except don't publish doc for pull requests"
+    else 
+		deploy
+	fi  
+}
+
+function clean { 
+	echo "cleaning _site folder"
+	if [ -f "${FILE_LOG}" ]; then rm -f ${FILE_LOG}; fi 
+	if [ -f "${FILE_PDF}" ]; then rm -f ${FILE_PDF}; fi 
+}
+
+function get_current_doc { 
+	echo "getting latest doc"
+	git clone --depth 0 $DEPLOY_REPO
+}
+
+function build_doc { 
+	echo "building doc"
+	mvn exec:java -Dexec.mainClass=io.github.oliviercailloux.plaquette_mido_soap.M1ApprBuilder
+}
+
+function deploy {
+	echo "deploying changes"
+	FILES="${FILE_LOG}"
+	if test -f "${FILE_PDF}"; then
+		FILES="${FILES} ${FILE_PDF}"		
+	fi
+	mv -f ${FILES} ${REPO}
+	cd ${REPO}
+	git config --global user.name "Travis CI"
+    git config --global user.email barnabe.geffroy@psl.eu
+	git add ${FILES}
+	git status
+	git commit -m "Lastest doc built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
+	git push $DEPLOY_REPO master
+}
+
+main
