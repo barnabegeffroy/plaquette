894263054bb9a1d8174263e91c7c7eb976a17af9
Barnabé Geffroy <barnabe.geffroy@psl.eu>
2020-05-01 09:25:49 +0200

    Try to push just logs without pdf

--- src/main/java/io/github/oliviercailloux/plaquette_mido_soap/M1ApprBuilder.java
+++ src/main/java/io/github/oliviercailloux/plaquette_mido_soap/M1ApprBuilder.java
@@ -51,7 +51,7 @@ public class M1ApprBuilder {
 	static public final String MAIN_MANAGER_2_PERSON_ID = "FRUAI0750736TPEIN711";
 
 	static public final String PROGRAM_IDENT = "PRA4AMIA-100";
-	static public final String PROGRAM_NAME = "Méthodes Informatiques Appliquées pour la Gestion des Entreprises - 1ère année de Master";
+	static public final String PROGRAM_NAME = "Méthodes Informatiques Appliquées pour la Gestion des Entreprises - 1ère année";
 	static public final String PROGRAM_ID_S1 = "FRUAI0750736TPRCPA4AMIA-100-S1";
 	static public final String PROGRAM_ID_S1_L1 = "FRUAI0750736TPRCPA4AMIA-100-S1L1";
 	static public final String S1_L1_NAME = "UE Obligatoires";
